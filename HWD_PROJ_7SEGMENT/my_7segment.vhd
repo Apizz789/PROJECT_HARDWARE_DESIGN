3594ad546602d87a44f716e1b9106f1d735b859c
62011019 <p.pluemapirak@gmail.com>
2022-05-10 09:28:52 +0700

    Update my_7segment.vhd

--- HWD_PROJ_7SEGMENT/my_7segment.vhd
+++ HWD_PROJ_7SEGMENT/my_7segment.vhd
@@ -45,6 +45,13 @@ end My_7Segment;
 architecture TodLongJaa of My_7Segment is
 
 begin
+A <= B0 OR B2 OR (B1 AND B3) OR (NOT B1 AND NOT B3);
+B <= (NOT B1) OR (NOT B2 AND NOT B3) OR (B2 AND B3);
+C <= B1 OR NOT B2 OR B3;
+D <= (NOT B1 AND NOT B3) OR (B2 AND NOT B3) OR (B1 AND NOT B2 AND B3) OR (NOT B1 AND B2) OR B0;
+E <= (NOT B1 AND NOT B3) OR (B2 AND NOT B3);
+F <= B0 OR (NOT B2 AND NOT B3) OR (B1 AND NOT B2) OR (B1 AND NOT B3);
+G <= B0 OR (B1 AND NOT B2) OR ( NOT B1 AND B2) OR (B2 AND NOT B3);
 
 
 end TodLongJaa;
