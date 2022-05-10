c5344d8b263b7032177b83b17643030861a44eb6
62011019 <p.pluemapirak@gmail.com>
2022-05-10 09:24:22 +0700

    7segment

---
+++ HWD_PROJ_7SEGMENT/my_7segment.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    09:23:41 05/10/2022 
+-- Design Name: 
+-- Module Name:    My_7Segment - TodLongJaa 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity My_7Segment is
+    Port ( B0 : in  STD_LOGIC;
+           B1 : in  STD_LOGIC;
+           B2 : in  STD_LOGIC;
+           B3 : in  STD_LOGIC;
+           A : out  STD_LOGIC;
+           B : out  STD_LOGIC;
+           C : out  STD_LOGIC;
+           D : out  STD_LOGIC;
+           F : out  STD_LOGIC;
+           G : out  STD_LOGIC);
+end My_7Segment;
+
+architecture TodLongJaa of My_7Segment is
+
+begin
+
+
+end TodLongJaa;
+
