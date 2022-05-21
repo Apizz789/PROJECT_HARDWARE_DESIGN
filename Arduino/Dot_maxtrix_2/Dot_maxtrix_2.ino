be1fcd357c288cd63faf5efc0b9a5380a083f533
62011019 <p.pluemapirak@gmail.com>
2022-05-21 12:36:13 +0700

    Create Dot_maxtrix_2.ino

---
+++ Arduino/Dot_maxtrix_2/Dot_maxtrix_2.ino
@@ -0,0 +1,84 @@
+#define REG_DIGIT(x)      (0x1+(x))
+#define REG_DECODE_MODE   (0x9)
+#define REG_INTENSITY     (0xA)
+#define REG_SCAN_LIMIT    (0xB)
+#define REG_SHUTDOWN      (0xC)
+#define REG_DISPLAY_TEST  (0xF)
+#include <SPI.h>
+
+const int CLK_PIN  = 10;  // ฟ้า
+const int CS_PIN   = 11;    // เขียว
+const int DIN_PIN  = 13;  // เหลือง
+
+
+
+
+void MAX7219_write_reg( uint8_t addr, uint8_t data ) {
+  digitalWrite( CS_PIN, LOW );
+  SPI.transfer( addr );
+  SPI.transfer( data );
+  digitalWrite( CS_PIN, HIGH );
+}
+
+
+
+void MAX7219_init(void) {
+  MAX7219_write_reg( REG_DECODE_MODE, 0x00 );  // decode mode: no decode for digits 0-7
+  MAX7219_write_reg( REG_INTENSITY, 0x07 );    // set intensity: 0x07=15/32
+  MAX7219_write_reg( REG_SCAN_LIMIT, 0x07 );   // scan limit: display digits 0-7
+  MAX7219_write_reg( REG_SHUTDOWN, 0x01 );     // shutdown: normal operation
+  MAX7219_write_reg( REG_DISPLAY_TEST, 0x00 ); // display test: no display test
+}
+
+void setup() {
+  SPI.begin();
+  SPI.setBitOrder( MSBFIRST );
+  SPI.setClockDivider( SPI_CLOCK_DIV16 );// 16MHz/16 -> 1MHz SCK frequency
+  SPI.setDataMode( SPI_MODE0 ); // use SPI mode 0
+  pinMode( CS_PIN, OUTPUT );
+  digitalWrite( CS_PIN, HIGH );
+  MAX7219_init();
+}
+
+void test_mode() {
+  MAX7219_write_reg( REG_SHUTDOWN, 0x01 );     // normal operation
+  MAX7219_write_reg( REG_DISPLAY_TEST, 0x01 ); // enter display test mode
+  delay(100);
+  MAX7219_write_reg( REG_DISPLAY_TEST, 0x00 ); // exit display test mode
+  MAX7219_write_reg( REG_SHUTDOWN, 0x00 );     // shutdown operation
+  delay(900);
+}
+
+const byte char_data[][8] = { // 'C', 'E'
+  { B00000000,
+    B10000001,
+    B10000001,
+    B10000001,
+    B10000001,
+    B10000001,
+    B11111111,
+    B00000000
+  },
+  { B00000000,
+    B10000001,
+    B10000001,
+    B10011001,
+    B10011001,
+    B10011001,
+    B11111111,
+    B00000000
+  }
+};
+
+void loop() {
+  test_mode();
+  MAX7219_write_reg( REG_SHUTDOWN, 0x01 );     
+  while (1) {
+    static uint8_t ch = 0;
+    for (uint8_t i = 0; i < 8; i++) {
+      MAX7219_write_reg( REG_DIGIT(i), char_data[ch][i] );
+    }
+    delay(500);
+    ch = (ch + 1) % 2;
+  }
+}
