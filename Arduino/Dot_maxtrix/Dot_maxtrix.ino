aa7866659d6dd222e8fe203f9409813aa9bfff12
62011019 <p.pluemapirak@gmail.com>
2022-05-21 12:31:28 +0700

    Update Dot_maxtrix.ino

--- Arduino/Dot_maxtrix/Dot_maxtrix.ino
+++ Arduino/Dot_maxtrix/Dot_maxtrix.ino
@@ -1,21 +1,26 @@
-#include <MD_Parola.h>
-#include <MD_MAX72xx.h>
-#include <SPI.h>
+//Mario's Ideas
+//MAX7219 - using Led Control library to display 8x8 bitmap
+#include <LedControl.h>
 
-#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 4
-#define CS_PIN      8
-MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+int DIN = 11;
+int CS = 7;
+int CLK = 13;
+LedControl lc=LedControl(DIN, CLK, CS,0);
+
+int Cat[8] ={B10001000,B11111000,B10101000,B01110001,B00100001,B01111001,B01111101,B10111110 };
+byte Apple [8]={B00011000,B00001000,B01110110,B11111111,B11111111,B11111111,B01111010,B00110100};
 
 void setup() {
-  myDisplay.begin();
-  myDisplay.setIntensity(0);
-  myDisplay.displayClear();
-  myDisplay.displayScroll("Hello World!", PA_CENTER, PA_SCROLL_LEFT, 100);
+  lc.shutdown(0,false);
+  lc.setIntensity(0,0);
+  lc.clearDisplay(0);
 }
-
-void loop() {
-  if (myDisplay.displayAnimate()) {
-    myDisplay.displayReset();
-  }
+  
+void loop(){
+  for(int i=0;i<8;i++) lc.setRow(0,i,Apple[i]);
+  delay(5000);
+  lc.clearDisplay(0);
+  for(int i=0;i<8;i++) lc.setRow(0,i,Cat[i]);
+  delay(5000);
+  lc.clearDisplay(0);
 }
