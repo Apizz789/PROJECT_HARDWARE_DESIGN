ef3cdd48dee22f528f5ffd886c9796f2503957e4
62011019 <p.pluemapirak@gmail.com>
2022-05-25 00:00:10 +0700

    s

--- Arduino/JOYSTICK_CONTROLLER/JOYSTICK_CONTROLLER.ino
+++ Arduino/JOYSTICK_CONTROLLER/JOYSTICK_CONTROLLER.ino
@@ -2,27 +2,28 @@ int SWPin5 = 5;
 int SWPin6 = 4;
 int buttonState5 = 0;
 int buttonState6 = 0;
+long int t1 = millis();
 void setup() {
+
   Serial.begin(9600);
-  pinMode (SWPin5, INPUT);
-  pinMode (SWPin6, INPUT);
+  pinMode (SWPin5, INPUT_PULLUP);
+    pinMode (SWPin6, INPUT_PULLUP);
 }
 
 void loop() {
   buttonState5 = analogRead(SWPin5);
   buttonState6 = analogRead(SWPin6);
-//  delay(120);
-  if ((buttonState5 == 1023 || buttonState6 == 1023)) {
-    if (buttonState5 == 1023 && delay==120) {
-      Serial.print("UP : ");
-      Serial.print(buttonState5);
+  //  delay(120);
 
-    }
-    else if(buttonState6 == 1023 && delay==120) {
-      Serial.print(" DOWN : ");
-      Serial.println(buttonState6);
-    }
+  if (buttonState5 < 1000) {
+    buttonState5 = 0;
+    Serial.print("NO DATA : ");
+    Serial.println(buttonState5);
+  } else {
+    Serial.print("UP : ");
+    Serial.println(buttonState5);
   }
+  
 
-
+ 
 }
